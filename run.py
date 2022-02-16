431509e2e0e4ec59891ef15185831ec0fcea4759
Astarag <athekunal@gmail.com>
2022-02-16 15:46:15 +0530

    Linting

--- run.py
+++ run.py
@@ -1,7 +1,9 @@
 from scraper import Verge_scraper
 
+
+# Checking linting
 try:
-    with Verge_scraper(keyword='',year = '2021',month='Feb') as bot:
+    with Verge_scraper(keyword="", year="2021", month="Feb") as bot:
         bot.land_req_page()
         bot.load_more()
         bot.info_extractor()
